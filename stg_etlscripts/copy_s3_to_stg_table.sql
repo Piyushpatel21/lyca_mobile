3d39f3eb4aeefdf21f2f1893df0c480e4df7babe
Hemanth Pasumarthi <hemanth.pasumarthi@cloudwick.com>
2019-12-15 09:18:18 +0000

    Update copy_s3_to_stg_table.sql used to ingest RRBS Data

--- stg_etlscripts/copy_s3_to_stg_table.sql
+++ stg_etlscripts/copy_s3_to_stg_table.sql
@@ -31,3 +31,72 @@ ALTER TABLE my_table ADD COLUMN processed_file_name varchar(256) NOT NULL DEFAUL
 
 
 
+
+---##Sample script to fix the extra coloumn issue ------------------------------
+copy ukdev.stg.temp_stg_VOICE_2019110723 from 's3://mis-dl-uk-eu-west-2-311477489434-dev-raw/RRBS/UK/VOICE/2019/11/07/VOICE_2019110723.cdr'
+iam_role 'arn:aws:iam::311477489434:role/Redshift-S3-uk-AllSubAccounts'
+delimiter ','
+removequotes
+emptyasnull
+blanksasnull
+maxerror 5;
+
+---## Script to ingest complete S3 folder---------------------------------------
+
+copy ukdev.stg.stg_rrbs_uk_voice from 's3://mis-dl-uk-eu-west-2-311477489434-dev-raw/RRBS/UK/VOICE/'
+iam_role 'arn:aws:iam::311477489434:role/Redshift-S3-uk-AllSubAccounts'
+csv
+delimiter ',';
+
+---## cdr_time_stamp validation which is used to create filename=---------------
+select cdr_time_stamp from ukdev.stg.stg_rrbs_uk_voice limit 50;
+
+
+---##Sample script to add new coloumns and update filename coloum ---------------
+ALTER TABLE ukdev.stg.stg_rrbs_uk_voice ADD COLUMN FileName varchar(50) NOT NULL DEFAULT 'nofilename';
+UPDATE ukdev.stg.stg_rrbs_uk_voice set FileName = concat('VOICE_',concat(substring(cdr_time_stamp,0,11),'.cdr')) where FileName='nofilename';
+ALTER TABLE ukdev.stg.stg_rrbs_uk_voice ADD COLUMN Created_Date datetime default sysdate;
+
+---##Sample script to alter table with sortkey ---------------
+ALTER TABLE ukdev.stg.stg_rrbs_uk_voice alter SORTKEY (FileName);
+
+---### Issues found during the ingestion 
+
+---1.-###S3 data from 's3://mis-dl-uk-eu-west-2-311477489434-dev-raw/RRBS/UK/VOICE/2019/11/07' has data from future dates
+cdr_time_stamp= 2019110800* = 2,549 records
+cdr_time_stamp= 2019110801* = 216 records
+cdr_time_stamp= 2019111302* = 24 records
+cdr_time_stamp= 2019111303* = 7 records
+
+---### Removed those rows using the following commands 
+delete from ukdev.stg.stg_rrbs_uk_voice where FileName='VOICE_2019110800.cdr';
+delete from ukdev.stg.stg_rrbs_uk_voice where FileName='VOICE_2019110801.cdr';
+delete from ukdev.stg.stg_rrbs_uk_voice where FileName='VOICE_2019111302.cdr';
+delete from ukdev.stg.stg_rrbs_uk_voice where FileName='VOICE_2019111303.cdr';
+
+
+---2.-###S3 data has same file names in two different folders only in RRBS-GPRS--
+/* e.g 
+RRBS/UK/GPRS/2019/11/06/GPRS_2019110623.cdr
+RRBS/UK/GPRS/2019/11/07/GPRS_2019110623.cdr
+*/
+
+/* e.g 
+RRBS/UK/GPRS/2019/11/05/GPRS_2019110523.cdr
+RRBS/UK/GPRS/2019/11/06/GPRS_2019110523.cdr
+*/
+
+/*
+RRBS/UK/GPRS/2019/11/03/GPRS_2019110323.cdr
+RRBS/UK/GPRS/2019/11/04/GPRS_2019110323.cdr
+*/
+/*
+RRBS/UK/GPRS/2019/11/02/GPRS_2019110223.cdr
+RRBS/UK/GPRS/2019/11/03/GPRS_2019110223.cdr
+*/
+/*
+RRBS/UK/GPRS/2019/11/01/GPRS_2019110123.cdr
+RRBS/UK/GPRS/2019/11/02/GPRS_2019110123.cdr
+*/
+
+
