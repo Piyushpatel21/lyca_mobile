05ff14b3687bab170d2cb462e99e205c796f3ce5
narenk <narendra.kumar@cloudwick.com>
2020-05-23 15:14:06 +0530

    Code re structruing for AWS

--- phase2/pyspark_etl/code/pythonlib/main/src/lycaSparkTransformation/LycaCommonETLLoad.py
+++ phase2/pyspark_etl/code/pythonlib/main/src/lycaSparkTransformation/LycaCommonETLLoad.py
@@ -81,42 +81,42 @@ def start_execution(args):
     propColumns = tf.srcSchema()
     duplicateData, lateUnique, normalUnique = tf.getSourceData(sparkSession, batch_id, propColumns.get("srcSchema"), propColumns.get("checkSumColumns"))
     lateUnique.show(20, False)
-    # normalDB, lateDB = tf.getDbDuplicate(sparkSession)
-    # normalNew, normalDuplicate = tf.getNormalCDR(normalUnique, normalDB)
-    # lateNew, lateDuplicate = tf.getLateCDR(lateUnique, lateDB)
-    # normalNewcnt = normalNew.count()
-    # lateNewCnt = lateNew.count()
-    # lateDuplicateCnt = lateDuplicate.count()
-    # duplicateDataCnt = duplicateData.count()
-    # normalDuplicateCnt = normalDuplicate.count()
-    # print("we are processing : normalNew={normalNew}, lateNew={lateNew}, lateDuplicate={lateDuplicate}, duplicateData={duplicateData}, normalDuplicate={normalDuplicate}"
-    #       .format(normalNew=normalNewcnt, lateNew=lateNewCnt, lateDuplicate=lateDuplicateCnt, duplicateData=duplicateDataCnt, normalDuplicate=normalDuplicateCnt))
-    # tf.writetoDataMart(sparkSession, normalNew, propColumns.get("tgtSchema"))
-    # tf.writetoLateCDR(sparkSession, lateNew, propColumns.get("tgtSchema"))
-    # tf.writetoDuplicateCDR(sparkSession, lateDuplicate, propColumns.get("tgtSchema"))
-    # tf.writetoDuplicateCDR(sparkSession, duplicateData, propColumns.get("tgtSchema"))
-    # tf.writetoDuplicateCDR(sparkSession, normalDuplicate, propColumns.get("tgtSchema"))
-    # tf.writetoDataMart(sparkSession, lateNew, propColumns.get("tgtSchema"))
-
-
-def parseArguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--run_date', help='run date required for trigger pipeline')
-    parser.add_argument('--batchID', help='run date required for trigger pipeline')
-    parser.add_argument('--module', help='module name required to process data')
-    parser.add_argument('--submodule', help='submodule name required to process data')
-    parser.add_argument('--configfile', help='application module level config file path')
-    parser.add_argument('--connfile', help='connection config file path')
-    parser.add_argument('--master', help='session for glue')
-    known_arguments, unknown_arguments = parser.parse_known_args()
-    arguments = vars(known_arguments)
-    if arguments:
-        if not (arguments.get('module') and arguments.get('submodule')):
-            print("--run_date --module, --submodule required for trigger pipeline")
-            sys.exit(1)
-    return arguments
-
-
-if __name__ == '__main__':
-    args = parseArguments()
-    start_execution(args)
+    normalDB, lateDB = tf.getDbDuplicate(sparkSession)
+    normalNew, normalDuplicate = tf.getNormalCDR(normalUnique, normalDB)
+    lateNew, lateDuplicate = tf.getLateCDR(lateUnique, lateDB)
+    normalNewcnt = normalNew.count()
+    lateNewCnt = lateNew.count()
+    lateDuplicateCnt = lateDuplicate.count()
+    duplicateDataCnt = duplicateData.count()
+    normalDuplicateCnt = normalDuplicate.count()
+    print("we are processing : normalNew={normalNew}, lateNew={lateNew}, lateDuplicate={lateDuplicate}, duplicateData={duplicateData}, normalDuplicate={normalDuplicate}"
+          .format(normalNew=normalNewcnt, lateNew=lateNewCnt, lateDuplicate=lateDuplicateCnt, duplicateData=duplicateDataCnt, normalDuplicate=normalDuplicateCnt))
+    tf.writetoDataMart(sparkSession, normalNew, propColumns.get("tgtSchema"))
+    tf.writetoLateCDR(sparkSession, lateNew, propColumns.get("tgtSchema"))
+    tf.writetoDuplicateCDR(sparkSession, lateDuplicate, propColumns.get("tgtSchema"))
+    tf.writetoDuplicateCDR(sparkSession, duplicateData, propColumns.get("tgtSchema"))
+    tf.writetoDuplicateCDR(sparkSession, normalDuplicate, propColumns.get("tgtSchema"))
+    tf.writetoDataMart(sparkSession, lateNew, propColumns.get("tgtSchema"))
+#
+#
+# def parseArguments():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--run_date', help='run date required for trigger pipeline')
+#     parser.add_argument('--batchID', help='run date required for trigger pipeline')
+#     parser.add_argument('--module', help='module name required to process data')
+#     parser.add_argument('--submodule', help='submodule name required to process data')
+#     parser.add_argument('--configfile', help='application module level config file path')
+#     parser.add_argument('--connfile', help='connection config file path')
+#     parser.add_argument('--master', help='session for glue')
+#     known_arguments, unknown_arguments = parser.parse_known_args()
+#     arguments = vars(known_arguments)
+#     if arguments:
+#         if not (arguments.get('module') and arguments.get('submodule')):
+#             print("--run_date --module, --submodule required for trigger pipeline")
+#             sys.exit(1)
+#     return arguments
+#
+#
+# if __name__ == '__main__':
+#     args = parseArguments()
+#     start_execution(args)
