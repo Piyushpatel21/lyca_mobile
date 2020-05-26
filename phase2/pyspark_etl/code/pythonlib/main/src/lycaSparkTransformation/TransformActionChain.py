aa39eb64a7cb12e3e9fa92d7e1c18d54c7d6ea64
Bhavin Tandel <bhavin.tandel@cloudwick.com>
2020-05-26 22:24:26 +0100

    derived columns generation for module sms

--- phase2/pyspark_etl/code/pythonlib/main/src/lycaSparkTransformation/TransformActionChain.py
+++ phase2/pyspark_etl/code/pythonlib/main/src/lycaSparkTransformation/TransformActionChain.py
@@ -11,7 +11,7 @@ from typing import Tuple
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType, StringType, StructField, StructType
 from datetime import datetime
-from lycaSparkTransformation.DataTransformation import DataTransformation
+from lycaSparkTransformation.DataTransformation import DataTransformation, SmsDataTransformation
 from lycaSparkTransformation.SchemaReader import SchemaReader
 from pyspark.sql import DataFrame
 from lycaSparkTransformation.JSONBuilder import JSONBuilder
@@ -76,9 +76,16 @@ class TransformActionChain:
             self.logger.info("***** reading source data from s3 *****")
             # file_list = self.redshiftprop.getFileList(sparkSession, batchid)
             file_list = ['new_sample_file.cdr']
-            # path ='/Users/bhavintandel/Documents/Documents/Cloudwick/Projects/lyca/lycamobile-etl-movements/phase2/pyspark_etl/code/pythonlib/test/resources/'
             path = self.property.get("sourceFilePath") + "/" + self.module.upper() + "/" + "UK" + "/" +self.subModule.upper() + "/" + self.run_date[:4] + "/" + self.run_date[4:6] + "/" + self.run_date[6:8] + "/"
-            df_source = self.trans.readSourceFile(self.sparkSession, path, srcSchema, batchid, checkSumColumns, file_list)
+            df_source_raw = self.trans.readSourceFile(self.sparkSession, path, srcSchema, batchid, checkSumColumns, file_list)
+            # Covert to target data type
+            if self.property.get("subModule") == "sms":
+                smsModuleTransformation = SmsDataTransformation()
+                df_source_with_datatype = smsModuleTransformation.convertTargetDataType(df_source_raw, srcSchema)
+                df_source = smsModuleTransformation.generateDerivedColumnsForSms(df_source_with_datatype)
+            else:
+                df_source = df_source_raw
+
             s3_batchreadcount = df_source.agg(py_function.count('batch_id').cast(IntegerType()).alias('s3_batchreadcount')).rdd.flatMap(lambda row: row).collect()
             s3_filecount = df_source.agg(py_function.countDistinct('filename').cast(IntegerType()).alias('s3_filecount')).rdd.flatMap(lambda row: row).collect()
             batch_status = 'Started'
