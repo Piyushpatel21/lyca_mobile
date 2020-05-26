89dfc225d9211f048565443aea4e2980a1e2eabf
Bhavin Tandel <bhavin.tandel@cloudwick.com>
2020-05-26 22:26:25 +0100

    Added test

--- phase2/pyspark_etl/code/pythonlib/test/lycaSparkTransformation/TestDataTransformation.py
+++ phase2/pyspark_etl/code/pythonlib/test/lycaSparkTransformation/TestDataTransformation.py
@@ -1,5 +1,5 @@
 from phase2.pyspark_etl.code.pythonlib.main.src.lycaSparkTransformation.SparkSessionBuilder import SparkSessionBuilder
-from phase2.pyspark_etl.code.pythonlib.main.src.lycaSparkTransformation.DataTransformation import DataTransformation
+from phase2.pyspark_etl.code.pythonlib.main.src.lycaSparkTransformation.DataTransformation import DataTransformation, SmsDataTransformation
 from pyspark.sql.types import *
 from pyspark.sql.functions import col, monotonically_increasing_id, lit
 import datetime
@@ -239,3 +239,81 @@ class TestTransformation:
         )
 
         assert actual_df.select('name', 'id').collect() == expected_df.collect()
+
+    def testSchemaAsString(self):
+        _schema = [StructField("name", StringType()),
+                   StructField("id", IntegerType()),
+                   StructField("_corrupt_record", StringType())]
+        str_schema = []
+        for elem in _schema:
+            str_schema.append(StructField(elem.name, StringType()))
+
+        df1 = self.spark.read.csv('../resources/test_error_file.csv', header=True, schema=StructType(str_schema))
+
+        actual_df = df1
+
+        print(actual_df.show())
+
+        expected_df = self.spark.createDataFrame(
+            [("foo", "1"),
+             ("bar", None),
+             ("foobar", "a")]
+        )
+
+        assert actual_df.select('name', 'id').collect() == expected_df.collect()
+
+    def testSchemaConversion(self):
+        data_tf_instance = DataTransformation()
+        _schema = [StructField("name", StringType()),
+                   StructField("id", IntegerType())]
+        str_schema = []
+        for elem in _schema:
+            str_schema.append(StructField(elem.name, StringType()))
+
+        df1 = self.spark.read.csv('../resources/test_error_file.csv', header=True, schema=StructType(str_schema))
+
+        actual_df = data_tf_instance.convertTargetDataType(df1, StructType(_schema))
+
+        print(actual_df.show())
+
+        expected_df = self.spark.createDataFrame(
+            [("foo", 1),
+             ("bar", None),
+             (None, None)]
+        )
+
+        assert StructType(_schema) == actual_df.schema
+
+
+class TestSmsTransformation:
+    sparkSessionBuild = SparkSessionBuilder().sparkSessionBuild()
+    spark = sparkSessionBuild.get("sparkSession")
+    smsTransformation = SmsDataTransformation()
+
+    def testgenerateDerivedColumnsForSms(self):
+
+        _schema = [StructField("name", StringType()),
+                   StructField("id", IntegerType()),
+                   StructField("msg_date", TimestampType()),
+                   StructField("free_zone_expiry_date", DateType())]
+        str_schema = []
+        for elem in _schema:
+            str_schema.append(StructField(elem.name, StringType()))
+
+        df1 = self.spark.read.csv('../resources/test_file.csv', header=True, schema=StructType(str_schema))
+
+        actual_df = self.smsTransformation.generateDerivedColumnsForSms(df1)
+
+        print(actual_df.show())
+        for elem in actual_df.schema:
+            print(elem.name + " : " + str(elem.dataType))
+
+        expected_df = self.spark.createDataFrame(
+            [("foo", "1", "20200226012705", "2020-05-25", 202002, datetime.date(2020, 2, 26), 20200226, 2020022601,
+              datetime.datetime(2020, 2, 26, 1, 27, 5), 202002, 20200226, 2020022601, 20200525),
+             ("bar", None, "20200329192705", None, 202003, datetime.date(2020, 3, 29), 20200329, 2020032919,
+              datetime.datetime(2020, 3, 29, 18, 27, 5), 202003, 20200329, 2020032918, None),
+             ("foobar", "a", None, None, None, None, None, None, None, None, None, None, None)]
+        )
+
+        assert actual_df.collect() == expected_df.collect()
