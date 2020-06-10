6c40d79b75af0f69d805e2b60ca7decf5894fa6c
Despoina Karna <despina.kar@cloudwick.com>
2020-06-10 15:51:49 +0100

    improvements

--- build/gitlab.sh
+++ build/gitlab.sh
@@ -27,6 +27,6 @@ git remote add code_commit https://git-codecommit.eu-west-2.amazonaws.com/v1/rep
 
 git remote -v
 
-git fetch && git checkout ${CI_COMMIT_REF_NAME} && git pull origin ${CI_COMMIT_REF_NAME}
+git fetch && git checkout origin ${CI_COMMIT_REF_NAME} && git pull origin ${CI_COMMIT_REF_NAME}
 printf "hash: $(git rev-parse HEAD) ci_ref: ${CI_COMMIT_REF_NAME}\n"
 git push code_commit ${CI_COMMIT_REF_NAME}
