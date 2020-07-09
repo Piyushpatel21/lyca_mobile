d51298adc4ac56417031435d3ff4f3fff5116089
Narendra Kumar <narendra.kumar@cloudwick.com>
2020-07-09 10:37:26 +0000

    Deploy ggsn fix to lycadev

--- phase2/pyspark_etl/GGSN_OTL/code/pythonlib/main/src/lycaSparkTransformation/LycaCommonETLLoad.py
+++ phase2/pyspark_etl/GGSN_OTL/code/pythonlib/main/src/lycaSparkTransformation/LycaCommonETLLoad.py
@@ -66,7 +66,7 @@ def start_execution(args):
     logger = sparkSessionBuild.get("logger")
     tf = TransformActionChain(sparkSession, logger, args.get('module'), args.get('submodule'),
                               configfile, connfile, run_date, prevDate, args.get('code_bucket'),
-                              args.get('source_file_path'))
+                              args.get('encoding'), args.get('source_file_path'))
     if not (args.get('run_date') and args.get('batchID')):
         batch_id = tf.getBatchID()
     else:
@@ -79,27 +79,32 @@ def start_execution(args):
     propColumns = tf.srcSchema()
     try:
         duplicateData, lateUnique, normalUnique, recordCount = tf.getSourceData(batch_id, propColumns.get("srcSchema"), propColumns.get("checkSumColumns"))
-        normalDB, lateDB,  = tf.getDbDuplicate()
-        normalNew, normalDuplicate, normalcdr_count, normalcdr_dupl_count = tf.getNormalCDR(normalUnique, normalDB, batch_id)
-        lateNew, lateDuplicate, latecdr_count, latecdr_dupl_count = tf.getLateCDR(lateUnique, lateDB, batch_id)
-        dfmetadata = recordCount.join(normalcdr_count, on='filename', how='left_outer') \
-                                .join(normalcdr_dupl_count, on='filename', how='left_outer') \
-                                .join(latecdr_count, on='filename', how='left_outer') \
-                                .join(latecdr_dupl_count, on='filename', how='left_outer') \
-                                .withColumnRenamed('filename', 'FILE_NAME') \
-                                .na.fill(0)
+        # normalDB, lateDB,  = tf.getDbDuplicate()
+        # normalNew, normalDuplicate, normalcdr_count, normalcdr_dupl_count = tf.getNormalCDR(normalUnique, normalDB, batch_id)
+        # lateNew, lateDuplicate, latecdr_count, latecdr_dupl_count = tf.getLateCDR(lateUnique, lateDB, batch_id)
+        # dfmetadata = recordCount.join(normalcdr_count, on='filename', how='left_outer') \
+        #                         .join(normalcdr_dupl_count, on='filename', how='left_outer') \
+        #                         .join(latecdr_count, on='filename', how='left_outer') \
+        #                         .join(latecdr_dupl_count, on='filename', how='left_outer') \
+        #                         .withColumnRenamed('filename', 'FILE_NAME') \
+        #                         .na.fill(0)
         # print("We are processing normalNew={normalNew}, lateNew={lateNew}, lateDuplicate={lateDuplicate}, "
         #       "duplicateData={duplicateData}, normalDuplicate={normalDuplicate}, lateNew={lateNew}"
         #       .format(normalNew=normalNew.count(), lateNew=lateNew.count(), lateDuplicate=lateDuplicate.count(),
         #               duplicateData=duplicateData.count(), normalDuplicate=normalDuplicate.count()))
         # dfmetadata.show(20, False)
-        tf.writeBatchFileStatus(dfmetadata, batch_id)
+        # tf.writeBatchFileStatus(dfmetadata, batch_id)
+        # tf.writetoDuplicateCDR(duplicateData, propColumns.get("tgtSchema"))
+        # tf.writetoDuplicateCDR(lateDuplicate, propColumns.get("tgtSchema"))
+        # tf.writetoDuplicateCDR(normalDuplicate, propColumns.get("tgtSchema"))
+        # tf.writetoDataMart(normalNew, propColumns.get("tgtSchema"))
+        # tf.writetoLateCDR(lateNew, propColumns.get("tgtSchema"))
+        # tf.writetoDataMart(lateNew, propColumns.get("tgtSchema"))
         tf.writetoDuplicateCDR(duplicateData, propColumns.get("tgtSchema"))
-        tf.writetoDuplicateCDR(lateDuplicate, propColumns.get("tgtSchema"))
-        tf.writetoDuplicateCDR(normalDuplicate, propColumns.get("tgtSchema"))
-        tf.writetoDataMart(normalNew, propColumns.get("tgtSchema"))
-        tf.writetoLateCDR(lateNew, propColumns.get("tgtSchema"))
-        tf.writetoDataMart(lateNew, propColumns.get("tgtSchema"))
+        tf.writetoDataMart(normalUnique, propColumns.get("tgtSchema"))
+        tf.writetoLateCDR(lateUnique, propColumns.get("tgtSchema"))
+        tf.writetoDataMart(lateUnique, propColumns.get("tgtSchema"))
+
         logger.info("ETL processing completed for batch - {batch_id}".format(batch_id=batch_id))
         tf.writeBatchStatus(batch_id, "Complete")
     except Exception as ex:
